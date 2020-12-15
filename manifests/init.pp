a19acf82baa13b3139561d619e366a42aff85203
mh <mh@immerda.ch>
2020-12-15 11:40:13 +0100

    make module EL 8 ready

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,27 @@
-# modules/mlocate/manifests/init.pp - manage mlocate stuff
+# manage mlocate stuff
 # Copyright (C) 2008 admin@immerda.ch
 # GPLv3
-
 class mlocate {
-  package{'mlocate':
+  package { 'mlocate':
     ensure => present,
+  } -> file { '/etc/updatedb.conf':
+    source => ["puppet:///modules/site_mlocate/updatedb/${facts['networking']['fqdn']}/updatedb.conf",
+                "puppet:///modules/site_mlocate/updatedb/${facts['os']['name']}/updatedb.conf.${facts['os']['release']['major']}",
+                "puppet:///modules/site_mlocate/updatedb/${facts['os']['name']}/updatedb.conf",
+                'puppet:///modules/site_mlocate/updatedb/updatedb.conf',
+                "puppet:///modules/mlocate/updatedb/${facts['os']['name']}/updatedb.conf.${facts['os']['release']['major']}",
+                "puppet:///modules/mlocate/updatedb/${facts['os']['name']}/updatedb.conf",
+                'puppet:///modules/mlocate/updatedb/updatedb.conf'],
+    owner  => root,
+    group  => 0,
+    mode   => '0644';
   }
 
-  file{'/etc/updatedb.conf':
-    source => [ "puppet:///modules/site_mlocate/updatedb/${::fqdn}/updatedb.conf",
-                "puppet:///modules/site_mlocate/updatedb/${::operatingsystem}/updatedb.conf.${::operatingsystemmajrelease}",
-                "puppet:///modules/site_mlocate/updatedb/${::operatingsystem}/updatedb.conf",
-                'puppet:///modules/site_mlocate/updatedb/updatedb.conf',
-                "puppet:///modules/mlocate/updatedb/${::operatingsystem}/updatedb.conf.${::operatingsystemmajrelease}",
-                "puppet:///modules/mlocate/updatedb/${::operatingsystem}/updatedb.conf",
-                'puppet:///modules/mlocate/updatedb/updatedb.conf' ],
-    require => Package['mlocate'],
-    owner   => root,
-    group   => 0,
-    mode    => '0644';
+  if $facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['major'],8) >= 0 {
+    service { 'mlocate-updatedb.timer':
+      ensure  => running,
+      enable  => true,
+      require => File['/etc/updatedb.conf'],
+    }
   }
 }
